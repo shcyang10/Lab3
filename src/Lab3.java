fbe3237a0c1ac6a3fb966ef02eafc5b0c94b2fe1
sean <seanhcyang10@gmail.com>
2019-07-29 22:53:21 -0400

    second commit

--- src/Lab3.java
+++ src/Lab3.java
@@ -16,27 +16,32 @@ public class Lab3 {
 		while (userChar == 'y') {
 			System.out.print("Please enter a number between 1 and 100: ");
 			num = scan.nextInt();
-		if (num %2 != 0 ) {
-			System.out.println(num + " Odd.");
-		}
-		else if (num %2 == 0 && num >=2 && num <=25){
-			System.out.println("Even and less than 25.");
+			if( num >= 1 && num <= 100) {
+				
+				if (num %2 != 0 && num < 60) {
+					System.out.println(num + " Odd.");
+				}
+				else if (num %2 == 0 && num >=2 && num <=25){
+					System.out.println("Even and less than 25.");
+				}
+				else if (num %2 == 0 && num >= 26 && num <=60) {
+					System.out.println("Even");
+				}
+				else if (num %2 == 0 && num >=60) {
+					System.out.println(num + " Even.");
+				}
+				else if (num %2 != 0 && num >=60) {
+					System.out.println(num + " Odd and over 60.");
 			}
-		else if (num %2 == 0 && num >= 26 && num <=60) {
-			System.out.println("Even");
-				}
-		else if (num %2 == 0 && num >=60) {
-			System.out.println(num + " Even.");
-					}
-		else if (num %2 != 0 && num >=60) {
-			System.out.println(num + " Odd.");
+				}else {
+			System.out.println("Invalid number, please enter a number between 1 and 100: ");
 		}
-		System.out.println("Continue? (y/n)");
-		scan.nextLine();
-		userChar = scan.next().charAt(0);
-				}
-		System.out.println("Bye!" + name);
+			System.out.println("Continue? (y/n)");
+			scan.nextLine();
+			userChar = scan.next().charAt(0);
 		
+		}
+		System.out.println("Bye!" + name);
 		scan.close();
 }
 }
